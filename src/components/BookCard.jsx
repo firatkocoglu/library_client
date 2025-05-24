61b796472e129acaab4750f3c9c4dd3a79528d1a
Fırat Koçoğlu <fratkocogl@gmail.com>
2025-05-24 12:25:17 +0300

    Add BookCard component

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,17 +1,20 @@
 function BookCard({ book }) {
-  <div className='book-card'>
-    <h2>{book.title}</h2>
-    <p>Author: {book.author}</p>
-    <p>Published: {book.year}</p>
-    <p>Genre: {book.genre}</p>
-    {book.available > 0 ? (
-      <button className='btn'>Loan</button>
-    ) : (
-      <button className='btn' disabled>
-        Not Available
-      </button>
-    )}
-  </div>;
+  return (
+    <div className='book-card border border-solid rounded-lg p-4 m-4 shadow-lg'>
+      <img src={book.image_url} alt={book.title} srcSet='' />
+      <h2>{book.title}</h2>
+      <p>Author: {book.author}</p>
+      <p>Published: {book.year}</p>
+      <p>Genre: {book.genre}</p>
+      {book.available > 0 ? (
+        <button className='btn'>Loan</button>
+      ) : (
+        <button className='btn' disabled>
+          Not Available
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default BookCard;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,25 +3,64 @@ import { useEffect } from 'react';
 import BookCard from './BookCard';
 
 function Books() {
+  const [loading, setLoading] = useState(false);
   const [books, setBooks] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchBooks = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`http://localhost:3000/books?page=${page}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setBooks(data.books);
+    } catch (error) {
+      console.error('Error fetching books:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    fetch('http://localhost:3000/books')
-      .then((response) => response.json())
-      .then((data) => setBooks(data));
-  }, []);
+    fetchBooks();
+  }, [page]);
 
   return (
-    <section>
+    <section className='books-section flex flex-col items-center justify-center'>
       <div>
         <h1>Books Available</h1>
-        <ul>
+        <ul className='books-list'>
           {books.map((book) => (
             <li key={book.id}>
               <BookCard book={book} />
             </li>
           ))}
         </ul>
+      </div>
+      <div className='page-buttons flex gap-4 justify-center items-center mt-4 mb-4 p-4'>
+        {page > 1 && (
+          <button
+            className='btn'
+            onClick={() => {
+              if (page > 1) {
+                setPage(page - 1);
+              }
+            }}
+          >
+            {page - 1}
+          </button>
+        )}
+        <button className='btn'>{page}</button>
+        <button
+          className='btn'
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
+          {page + 1}
+        </button>
       </div>
     </section>
   );
